b30dd51d1c08226b43c3b0ec2a2c75bf7c8e8c35
roshan.kumar <roshan.kumar@scientificgames.com>
2021-11-24 20:50:56 +0530

    updated logic

--- cs-clrs/src/ch02/insertion-sort.py
+++ cs-clrs/src/ch02/insertion-sort.py
@@ -1,8 +1,8 @@
 def insertion_sort(A):
-    for j in range(1, len(A)-1):
+    for j in range(1, len(A)):
         key=A[j]
         i=j-1
-        while i>0 and A[i]>key:
+        while (i>=0 and A[i]>key):
             A[i+1]=A[i]
             i=i-1
         A[i+1]=key
