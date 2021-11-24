0e1d6c1aa92f7bff1ab43cfe2a5c80753926d052
roshan.kumar <roshan.kumar@scientificgames.com>
2021-11-24 18:39:18 +0530

    insertion-sort first commit

---
+++ cs-clrs/src/ch02/insertion-sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(A):
+    for j in range(1, len(A)-1):
+        key=A[j]
+        i=j-1
+        while i>0 and A[i]>key:
+            A[i+1]=A[i]
+            i=i-1
+        A[i+1]=key
+    return A
+
+A = [5,32,2,9,-2,99]
+print(insertion_sort(A))
